66122f171600a9febe545a14ca3e4f0e030b6cc1
Pierric Gimmig <pierric.gimmig@gmail.com>
2022-12-31 16:31:36 -0800

    Initial commit

---
+++ src/hijack/OrbitAsm.h
@@ -0,0 +1,59 @@
+//-----------------------------------
+// Copyright Pierric Gimmig 2013-2017
+//-----------------------------------
+#pragma once
+
+#include <windows.h>
+#include "OrbitAsmC.h"
+
+//-----------------------------------------------------------------------------
+struct OrbitProlog
+{
+    OrbitProlog(){ memset( this, 0, sizeof( OrbitProlog ) ); }
+    void Init();
+    Prolog m_Data;
+};
+
+//-----------------------------------------------------------------------------
+struct OrbitEpilog
+{
+    OrbitEpilog(){ memset( this, 0, sizeof( OrbitEpilog ) ); }
+    void Init();
+    Epilog m_Data;
+};
+
+#pragma pack(push, 1)
+//-----------------------------------------------------------------------------
+struct OrbitSSEContext
+{
+    _M128A xmm0;
+    _M128A xmm1;
+    _M128A xmm2;
+    _M128A xmm3;
+    _M128A xmm4;
+    _M128A xmm5;
+    _M128A xmm6;
+    _M128A xmm7;
+    _M128A xmm8;
+    _M128A xmm9;
+    _M128A xmm10;
+    _M128A xmm11;
+    _M128A xmm12;
+    _M128A xmm13;
+    _M128A xmm14;
+    _M128A xmm15;
+};
+#pragma pack(pop)
+
+#ifdef _WIN64
+//-----------------------------------------------------------------------------
+extern "C" void OrbitGetSSEContext( OrbitSSEContext * a_Context );
+extern "C" void OrbitSetSSEContext( OrbitSSEContext * a_Context );
+extern "C" void OrbitPrologAsm();
+extern "C" void OrbitPrologOnlyAsm();
+extern "C" void OrbitEpilogAsm();
+#else
+//-----------------------------------------------------------------------------
+void OrbitPrologAsm();
+void OrbitEpilogAsm();
+#endif
