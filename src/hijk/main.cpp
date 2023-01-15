fa9d64bef8aeb43ecc51d1888a188b0e1f27dd41
Pierric Gimmig <pierric.gimmig@gmail.com>
2023-01-15 09:53:33 -0800

    Remove unused headers

--- src/hijk/main.cpp
+++ src/hijk/main.cpp
@@ -1,22 +1,17 @@
-#include <Windows.h>
-#include <debugapi.h>
-
 #include <iostream>
 
 #include "hijk/hijk.h"
-
-static volatile int g_count;
 
 __declspec(noinline) void MyFunction(int a, float b) {
   std::cout << "a: " << a << " b: " << b << std::endl;
 }
 
 extern "C" {
+
 void Prolog(void* original_function, struct Hijk_PrologContext* prolog_context) {
   std::cout << "User Prolog!\n";
 }
 void Epilog(void* original_function, struct Hijk_EpilogContext* epilog_context) {
-  ++g_count;
   std::cout << "User Epilog!\n";
 }
 }
